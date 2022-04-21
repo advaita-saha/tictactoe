095c8eedd7a84f6170f9d3a3888c8a5298fe2e10
Advaita Saha <adv11sphs@gmail.com>
2022-04-21 18:40:35 +0530

    text changed to images

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -4,7 +4,10 @@ import "../App.css"
 function Square({val, chooseSquare}){
     return (
         <div className="square" onClick={chooseSquare}>
-            {val}
+            
+            {val == "X" 
+            ? <img className="img-s" src="https://pngimg.com/uploads/hammer/hammer_PNG1124.png" alt="X"/> 
+            : val == "O" ? <img className="img-s" src="https://www.freeiconspng.com/uploads/soccer-ball-png-9.png" alt="O"/> : val }
         </div>
     )
 }
